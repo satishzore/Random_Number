1da86b14a20a67e41da1835134614a03d4626001
Satish Zore <satishzore056@gmail.com>
2023-04-05 16:06:08 +0530

    First Commit

---
+++ satish.java
@@ -0,0 +1,13 @@
+import java.lang.Math;  
+public class RandomNumberExample1  
+{   
+public static void main(String args[])   
+{   
+ 
+for(int i=1;i<=10;i++)
+{
+    System.out.println(" Random Number: " + Math.random());
+}
+
+}   
+}  
